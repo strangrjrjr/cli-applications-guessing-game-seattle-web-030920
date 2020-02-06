9a4a670b8d08400bce661d86c58b5275640d0e18
Roger Robertson Jr <rjrjr@pm.me>
2020-02-06 22:32:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   
   guess = gets.chomp
   
-  if guess == num
+  if guess.to_i == num
     puts "You guessed the correct number!"
   elsif guess == 'exit'
     puts "Goodbye!"    

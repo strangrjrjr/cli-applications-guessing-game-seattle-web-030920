1c061edd7e669e8de87d0e1cc87e193ec1ef9ee3
Roger Robertson Jr <rjrjr@pm.me>
2020-02-06 22:27:18 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,10 +2,10 @@
 def run_guessing_game
   num = rand(6) + 1
   guess = gets.chomp
-  if guess == 'exit'
-    puts 'Goodbye!'
-  elsif guess == num
+  if guess == num
     puts "You guessed the correct number!"
+  elsif guess == 'exit'
+    puts "Goodbye!"    
   else
     puts "Sorry! The computer guessed #{num}."
   end
